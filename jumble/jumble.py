5c237d85d794380ca054ca8a6519ce17f00b482b
chwilk <chwilk@gmail.com>
2017-11-14 12:50:16 -0600

    Word length depth limit
    Added a limit based on input word length to avoid searching forever

--- jumble/jumble.py
+++ jumble/jumble.py
@@ -1,4 +1,8 @@
 #! /usr/bin/env python
+import os
+import sys
+
+myrange = 5
 
 def lensort(x,y):
     c=cmp(len(x),len(y))
@@ -7,12 +11,12 @@ def lensort(x,y):
     else:
         return(cmp(x,y))
 
-def divide(jumb):
+def divide(jumb, limit):
     """Recursive function to search for substrings in dictionary"""
     words=[]
-    if len(jumb) > 3:
+    if len(jumb) > limit:
 	for i in range(0,len(jumb)):
-            words.extend(divide(jumb[:i]+jumb[i+1:]))
+            words.extend(divide(jumb[:i]+jumb[i+1:], limit))
     if alpha.has_key(jumb):
         words.extend(alpha[jumb])
     return(sorted(list(set(words)),lensort))
@@ -21,21 +25,25 @@ def divide(jumb):
 
 if __name__ == "__main__":
     alpha=dict()
-    with open('/usr/share/dict/words', 'r') as dict:
+    wordlist=os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), 'TWL06.db')
+    #wordlist="/usr/share/dict/words"
+    with open(wordlist, 'r') as dict:
         for word in dict:
-	    word=word.rstrip()
+	    word=word.rstrip().lower()
             if word.islower():
                 key=''.join(sorted(word))
                 if alpha.has_key(key):
                     alpha[key].append(word)
                 else:
                     alpha[key]=[word]
+    print "Read {} words\n".format(len(alpha))
     print "Type 'q' to exit\n"
     jumb=''
     while(jumb != 'q'):
         try:
             jumb = str(raw_input("Jumble> ")).rstrip()
-            for i in divide(''.join(sorted(jumb))):
+            limit = max(3, len(jumb)-myrange)
+            for i in divide(''.join(sorted(jumb)), limit):
                 print i
         except KeyboardInterrupt:
             print "Exiting"
